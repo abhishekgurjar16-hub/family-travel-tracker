c134c1c8dde3958b06155669bd5b00e0b30cb7aa
Abhishek <abhishekgurjar.1.6.2.4@gmail.com>
2026-09-15 15:03:43 +0530

    Handle empty users and select new user

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
